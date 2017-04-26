527e2648e231921ec42d302a1c91f00be94ca37f
sasalicat <waimaoisme@126.com>
2017-04-26 12:26:52 +0800

    终于把累死人的Contriler基本控制委派的界面完成了

--- Assets/script(net)/NetPlayerControler.cs
+++ Assets/script(net)/NetPlayerControler.cs
@@ -9,6 +9,107 @@ public class NetPlayerControler : MonoBehaviour,Controler {
     private int lastZ=0;
     private float timeInterval = 0;
     private Dictionary<string, KeyCode> keySetting;
+    _on_left_down on_left_down;
+    _on_right_down on_right_down;
+    _on_middle_down on_middle_down;
+    _on_key1_down on_key1_down;
+    _on_key2_down on_key2_down;
+    _on_key3_down on_key3_down;
+    _on_key4_down on_key4_down;
+    _on_key5_down on_key5_down;
+    _on_keyup_down on_keyup_down;
+    _on_keyup_ing on_keyup_ing;
+    _on_keyup_up on_keyup_up;
+    _on_keyleft_down on_keyleft_down;
+    _on_keyleft_ing on_keyleft_ing;
+    _on_keyleft_up on_keyleft_up;
+    _on_keydown_down on_keydown_down;
+    _on_keydown_ing on_keydown_ing;
+    _on_keydown_up on_keydown_up;
+    _on_keyright_down on_keyright_down;
+    _on_keyright_ing on_keyright_ing;
+    _on_keyright_up on_keyright_up;
+
+    public _on_key1_down get_on_key1_down()
+    {
+        return on_key1_down;
+    }
+    public _on_key2_down get_on_key2_down()
+    {
+        return on_key2_down;
+    }
+    public _on_key3_down get_on_key3_down()
+    {
+        return on_key3_down;
+    }
+    public _on_key4_down get_on_key4_down()
+    {
+        return on_key4_down;
+    }
+    public _on_key5_down get_on_key5_down()
+    {
+        return on_key5_down;
+    }
+    public _on_keydown_down get_on_keydown_down()
+    {
+        return on_keydown_down;
+    }
+    public _on_keydown_ing get_on_keydown_ing()
+    {
+        return on_keydown_ing;
+    }
+    public _on_keydown_up get_on_keydown_up()
+    {
+        return on_keydown_up;
+    }
+    public _on_keyleft_down get_on_keyleft_down()
+    {
+        return on_keyleft_down;
+    }
+    public _on_keyleft_ing get_on_keyleft_ing()
+    {
+        return on_keyleft_ing;
+    }
+    public _on_keyleft_up get_on_keyleft_up()
+    {
+        return on_keyleft_up;
+    }
+    public _on_keyright_down get_on_keyright_down()
+    {
+        return on_keyright_down;
+    }
+    public _on_keyright_ing get_on_keyright_ing()
+    {
+        return on_keyright_ing;
+    }
+    public _on_keyright_up get_on_keyright_up()
+    {
+        return on_keyright_up;
+    }
+    public _on_keyup_down get_on_keyup_down()
+    {
+        return on_keyup_down;
+    }
+    public _on_keyup_ing get_on_keyup_ing()
+    {
+        return on_keyup_ing;
+    }
+    public _on_keyup_up get_on_keyup_up()
+    {
+        return on_keyup_up;
+    }
+    public _on_left_down get_on_left_down()
+    {
+        return on_left_down;
+    }
+    public _on_middle_down get_on_middle_down()
+    {
+        return on_middle_down;
+    }
+    public _on_right_down get_on_right_down()
+    {
+        return on_right_down;
+    }
     void Start()
     {
         GameObject temp = GameObject.Find("keyTabel");
--- Assets/script/Controler.cs
+++ Assets/script/Controler.cs
@@ -1,11 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//基础按键事件---------------------------------------------
+              /*技能按键*/
+public delegate void _on_left_down(Vector3 mousePos);//鼠标左键按下
+public delegate void _on_right_down(Vector3 mousePos);//鼠标右键按下
+public delegate void _on_middle_down(Vector3 mousePos);//鼠标中键按下
+public delegate void _on_key1_down(Vector3 mousePos);
+public delegate void _on_key2_down(Vector3 mousePos);
+public delegate void _on_key3_down(Vector3 mousePos);
+public delegate void _on_key4_down(Vector3 mousePos);
+public delegate void _on_key5_down(Vector3 mousePos);
+              /*移动按键*/
+public delegate void _on_keyup_down();//按下 上键
+public delegate void _on_keyup_ing();//按住 上键
+public delegate void _on_keyup_up();//松开 上键
+public delegate void _on_keyright_down();
+public delegate void _on_keyright_ing();
+public delegate void _on_keyright_up();
+public delegate void _on_keydown_down();
+public delegate void _on_keydown_ing();
+public delegate void _on_keydown_up();
+public delegate void _on_keyleft_down();
+public delegate void _on_keyleft_ing();
+public delegate void _on_keyleft_up();
+//被动效果触发事件----------------------------------------
 public delegate void on_attack();
 public delegate void on_take_damage();
 public delegate void up_press_on();
 public interface Controler
 {
+    _on_left_down get_on_left_down();
+    _on_right_down get_on_right_down();
+    _on_middle_down get_on_middle_down();
+    _on_key1_down get_on_key1_down();
+    _on_key2_down get_on_key2_down();
+    _on_key3_down get_on_key3_down();
+    _on_key4_down get_on_key4_down();
+    _on_key5_down get_on_key5_down();
+    /*移动按键*/
+    _on_keyup_down get_on_keyup_down();
+    _on_keyup_ing get_on_keyup_ing();
+    _on_keyup_up get_on_keyup_up();
+    _on_keyright_down get_on_keyright_down();
+    _on_keyright_ing get_on_keyright_ing();
+    _on_keyright_up get_on_keyright_up();
+    _on_keydown_down get_on_keydown_down();
+    _on_keydown_ing get_on_keydown_ing();
+    _on_keydown_up get_on_keydown_up();
+    _on_keyleft_down get_on_keyleft_down();
+    _on_keyleft_ing get_on_keyleft_ing();
+    _on_keyleft_up get_on_keyleft_up();
 
 }
