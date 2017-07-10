8f0ebfccd113b9b7992f16f4e9f66f74e8ef4f4e
yuanieen <yuanieen@gmail.com>
2017-07-10 18:50:07 +0800

    突击手动画树

---
+++ Assets/role(net)/突击手/mis_tjs_atk.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mis_tjs_atk : Missile{
+
+    public float m_liveTime;
+    public float juli;
+    public float zhuangtai;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector3 fei = this.transform.position;
+        Vector3 zhujiao = Creater.transform.position;
+        Vector3 direction = fei - zhujiao;
+        //juli = (float)System.Math.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.z, 2));
+        /*if (zhuangtai == 1)
+        {
+            Speed = m_liveTime * 15;
+            m_liveTime -= Time.deltaTime;
+
+            transform.Translate(0, Speed * Time.deltaTime, 0);
+        }
+
+        if (m_liveTime <= 0)
+        {
+            zhuangtai = 2;
+        }
+
+        if (zhuangtai == 2)
+        {
+            Speed = Speed + 1;
+            this.transform.forward = -direction;
+            this.transform.eulerAngles = new Vector3(0, 0, this.transform.eulerAngles.z);
+            transform.Translate(0, Speed * Time.deltaTime, 0, Space.Self);
+        }*/
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject != Creater)
+        {
+            //Debug.Log(other.name);
+            //Debug.Log(" on trigger enter 2D");
+            RoleState role = other.gameObject.GetComponent<RoleState>();
+            if (role != null)
+                role.TakeDamage(Damage);
+
+        }
+    }
+}
---
+++ Assets/role(net)/突击手/tjs_attack.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tjs_attack : MonoBehaviour, CDEquipment
+{
+
+    public const float CD = 0.5f;//0.5f;
+    public const int BaseDamage = 50;
+    public const float BaseStiff = 0.25f;
+
+    public float CDTime = 0;
+    public sbyte index;
+    const short selfMissileNo = 0;
+    private GameObject missilePraf;//暫存總missileTable內得到的預設體
+    private RoleState selfState;
+    public Text Label;
+
+    //實做Equipment介面-------------------------------------------------------
+    public sbyte No
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    public sbyte selfIndex
+    {
+        get
+        {
+            return index;
+        }
+
+        set
+        {
+            index = value;
+        }
+    }
+
+    public sbyte Kind//本技能属于主动技能所以kind为 PASSIVE_SKILL
+    {
+        get
+        {
+            return EquipmentTable.PASSIVE_SKILL;
+            //return EquipmentTable.ON_TAKE_DAMAGE;
+        }
+    }
+    //實做CDEquipment介面----------------------------------------------
+    public void setTime(float time)
+    {
+        CDTime -= time;//減少CD時間
+    }
+    public bool CanUse
+    {
+        get
+        {
+            Debug.Log(" in can use CDTime is" + CDTime);
+            return (CDTime <= 0);//如果CDTime小於0代表技能可以使用
+        }
+    }
+    public uint Consumption
+    {
+        get
+        {
+            return 0;//因為是攻擊所以無消耗
+        }
+    }
+    //----------------------------------------------------------------------
+
+
+    public void trigger(Dictionary<string, object> args)
+    {
+
+        getVector getVector = GameObject.Find("keyTabel").GetComponent<getVector>();
+        Vector3 origenPlayerPosition = (Vector3)args["PlayerPosition"];//施放技能時玩家位置
+        Vector3 mousePosition = (Vector3)args["MousePosition"];//施放技能時鼠標點擊位置
+        //使用getOriginalInitPoint得到技能在client端创建物件的正确位置
+        Vector3 tragetPos = getVector.getOriginalInitPoint(origenPlayerPosition, mousePosition, new Vector3(0, -1, 0));//獲得相對座標
+        //制造子弹物件
+        Vector3 direction = mousePosition - origenPlayerPosition;
+        Debug.Log(direction);
+        //GameObject newone = Instantiate(missilePraf, tragetPos, this.transform.rotation);
+        //missilePraf.transform.forward = direction;
+        //missilePraf.transform.eulerAngles = new Vector3(0, 0, missilePraf.transform.eulerAngles.z);
+        
+        GameObject newone = Instantiate(missilePraf, tragetPos, Quaternion.Euler(direction));
+        newone.transform.forward = direction;
+        Debug.Log(newone.transform.forward);
+        //修改子弹物件携带的子弹脚本
+        Missile missile = newone.GetComponent<Missile>();
+        missile.Creater = gameObject;
+        //创建伤害物件
+        int num = (int)(BaseDamage + BaseDamage * ((float)selfState.selfdata.power / 100));
+        float stiff = BaseStiff + BaseStiff * (((float)selfState.selfdata.stiffable) / 100);
+        missile.Damage = new damage(1, num, stiff, false, false, gameObject);
+
+
+        CDTime = CD;//技能冷卻
+        //Debug.Log("in trigger CDTime is" + CDTime);
+
+
+    }
+
+
+    public void onInit(MissileTable table, RoleState state, AnimatorTable anim)
+    {
+        //初始化赋值
+        missilePraf = table.MissileList[2];
+        this.selfState = state;
+    }
+}
