9080779059adbf8dce690a5d48b3d99aa5e261e1
yuanieen <yuanieen@gmail.com>
2017-07-12 19:47:48 +0800

    猎人部分

--- Assets/role(net)/突击手/NewBehaviourScript.cs
+++ Assets/role(net)/突击手/NewBehaviourScript.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ChangeColor : MonoBehaviour {
 
+    public SpriteRenderer s;
 	// Use this for initialization
 	void Start () {
-		
+        s = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        s.color = new Color(255, 255, 255, 0);
 	}
 	
 	// Update is called once per frame
