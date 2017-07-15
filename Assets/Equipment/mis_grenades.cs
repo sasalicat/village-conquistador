8b4518b4a977968298576e6b474deb47ce1b34a2
kiam <kiam04@hotmail.com>
2017-07-15 13:00:09 +0800

    完成手雷

--- Assets/Equipment/mis_grenades.cs
+++ Assets/Equipment/mis_grenades.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,30 +7,65 @@ public class mis_grenades : Missile
 {
     private Vector3 vspeed;
     private Animator anim;
+    public const int limitDistance = 15;
+
+    public Vector3 origenPlayerPosition;
+    public Vector3 mousePosition;
+    public bool boom = false;
+    public GameObject missilePraf2;
+    GameObject newone;
 
     // Use this for initialization
     void Start()
     {
+        anim = GetComponent<Animator>();
 
-        vspeed = new Vector3(0, Speed, 0);
+        Vector3 position = (mousePosition - origenPlayerPosition) + origenPlayerPosition;
+        
+        if ((mousePosition - origenPlayerPosition).magnitude < limitDistance)
+        {
+            transform.DOMove(mousePosition, 1f, false).OnComplete(() => getSkill2(mousePosition)).SetEase(Ease.OutQuart);
+        }
+        else
+        {
+            //向量 = 終點 - 起始點
+            //起始點 = 向量 - 終點
+            //終點 = 向量 + 起始點
+            //((終點 - 起始點)) 變單位向量
+            //終點 = 長度 * (終點 - 起始點).normalized + 起始點
+            Vector3 destination = (limitDistance * (mousePosition - origenPlayerPosition).normalized) +origenPlayerPosition;
+            transform.DOMove(destination, 1f, false).OnComplete(() => getSkill2(destination)).SetEase(Ease.OutQuart);
+        }
     }
+
+    void getSkill2(Vector3 destination)
+    {
+        newone = Instantiate(missilePraf2, destination, transform.rotation);
+        Missile missile = newone.GetComponent<Missile>();
+        missile.Creater = this.Creater;
+        missile.Damage = this.Damage;
+        newone.transform.localScale = new Vector3(2,2,1);
+        Destroy(gameObject);
+    }
+
     void Update()
     {
-        transform.Translate(vspeed * Time.deltaTime);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        RoleState role = other.gameObject.GetComponent<RoleState>();
-        if (role != null)
-            role.TakeDamage(Damage);
-
-        anim = GetComponent<Animator>();
-        anim.SetBool("boom", true);
+        boom = anim.GetBool("boom");
+        if (boom == true)
+        {
+            RoleState role = other.gameObject.GetComponent<RoleState>();
+            if (role != null)
+                role.TakeDamage(Damage);
+            Debug.Log("Boom actity");
+        }
     }
 
     public void getAnimation()
     {
-        Debug.Log("12312312323");
-        Destroy(gameObject);
+        
+        Destroy(newone);
     }
 }
