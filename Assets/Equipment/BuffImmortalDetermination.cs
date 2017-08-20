82a66d405bc6abab9ce4ea3a2576a47cd9d5d221
kiam123 <kiam125@gmail.com>
2017-08-20 21:49:54 +0800

    還沒完成不滅決心

--- Assets/Equipment/BuffImmortalDetermination.cs
+++ Assets/Equipment/BuffImmortalDetermination.cs
@@ -1,42 +1,41 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class buffForTask : Buff
+public class BuffImmortalDetermination : Buff
 {
     public override float Duration
     {
         get
         {
-            return 8;
+            return 10;
         }
     }
 
 
-    public override bool onInit(RoleState role, Buff[] Repetitive,MissileTable mis)
+    public override bool onInit(RoleState role, Buff[] Repetitive, MissileTable mis)
     {
         Debug.Log("buffForTask onInit");
         if (Repetitive == null)
         {
-            Debug.Log("onInit Enter Null");
-            role.canMove = false;
+            role.canBeKill = false;
         }
         return true;
     }
 
     public override void onRemove(RoleState role)
     {
-        role.canMove = true;
+        role.canBeKill = true;
     }
     public override void onIntarvel(RoleState role, float timeBetween)
     {
         base.onIntarvel(role, timeBetween);
         Debug.Log("buff ing");
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
---
+++ Assets/Equipment/ImmortalDetermination.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmortalDetermination : MonoBehaviour, CDEquipment
+{
+    public const float CD = 5f;//4f;
+    public const int BaseDamage = 100;//80
+    public const float BaseStiff = 0.5f;
+
+    public float CDTime = 0;
+    public sbyte index;
+    private RoleState selfState;
+    //實做Equipment介面-------------------------------------------------------
+    public sbyte No
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    public sbyte selfIndex
+    {
+        get
+        {
+            return index;
+        }
+
+        set
+        {
+            index = value;
+        }
+    }
+
+    public sbyte Kind//本技能属于主动技能所以kind为 PASSIVE_SKILL
+    {
+        get
+        {
+            return EquipmentTable.PASSIVE_SKILL;
+        }
+    }
+    //實做CDEquipment介面----------------------------------------------
+    public void setTime(float time)
+    {
+        CDTime -= time;//減少CD時間
+    }
+    public bool CanUse
+    {
+        get
+        {
+            Debug.Log(" in can use CDTime is" + CDTime);
+            return (CDTime <= 0);//如果CDTime小於0代表技能可以使用
+        }
+    }
+    public uint Consumption
+    {
+        get
+        {
+            return 0;//因為是攻擊所以無消耗
+        }
+    }
+
+    public bool Designated
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    //----------------------------------------------------------------------
+
+
+    public void trigger(Dictionary<string, object> args)
+    {
+        ((GameObject)args["Traget"]).GetComponent<Controler>().addBuffByNo(3);
+    }
+
+    public void onInit(MissileTable table, RoleState state, AnimatorTable anim)
+    {
+
+    }
+}
